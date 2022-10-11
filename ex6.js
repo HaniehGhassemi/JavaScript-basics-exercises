0c693b1c861c6df49c626fb13e33e8eb3ffef863
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2022-10-11 09:57:09 +0330

    Exercise 6
    for Bitwise OR Operator

--- ex6.js
+++ ex6.js
@@ -2,3 +2,11 @@
 //if user is male or under 18 years old show error message
 //else show welcome message
 
+let UserAge = prompt("Enter your age:");
+let UserGender = prompt("Enter your gender:", "Female or Male");
+
+if(UserAge < 18 || UserGender == "Male") {
+    alert("You can't acsess to this part of website");
+} else {
+    alert("Welcom!");
+}
