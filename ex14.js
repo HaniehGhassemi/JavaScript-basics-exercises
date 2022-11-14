2bd1a20766e9db6558ba11244e1065916a61f212
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2022-11-14 09:04:12 +0330

    Exercise 14
    for loop exercise with even numbers

---
+++ ex14.js
@@ -0,0 +1,16 @@
+//Print even numbers between 0 and 100
+
+let evenNumbers = 0;
+for (i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        evenNumbers = i;
+        console.log(evenNumbers);
+
+    }
+}
+
+//also u can write like this :
+
+// for (i = 0; i <= 100;  i += 2) {
+// console.log(i);
+// }
