759bd8f794636d783c8c7a9f77eb9a8a685dae3f
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2022-11-14 09:03:23 +0330

    Exercise 13
    for loop exercise

---
+++ ex13.js
@@ -0,0 +1,8 @@
+//Get a string from user
+//Print the letters of each index of string
+
+let stringInput = prompt("Put a string");
+
+for (let i = 0; i < stringInput.length; i++) {
+    console.log(stringInput[i]);
+}
